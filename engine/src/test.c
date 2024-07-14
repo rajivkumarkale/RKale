97215792a4f6e4110a8ac6ab479d82c95bef6161
Rajiv Kumar Kale <rajivkumarkale@gmail.com>
2024-07-14 17:06:37 +0530

    project initial setup for windows

---
+++ engine/src/test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "test.h"
+
+void print_int(i32 i) {
+    printf("The integer is: %i", i);
+}
---
+++ engine/src/test.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "defines.h"
+
+RAPI void print_int(i32 i);
